3b38f1993bac46166acbaf71997e25f29f89009c
Andrew Wei <weiandrew2012@gmail.com>
2022-10-02 18:13:08 -0700

    implementing tweens

--- Assets/Scripts/NoteEmitter.cs
+++ Assets/Scripts/NoteEmitter.cs
@@ -26,13 +26,15 @@ public class NoteEmitter : MonoBehaviour
     // Declares note Detector
     public GameObject detector;
     // distance for input to be valid
-    public const float validInputDistance = 1f;
+    public const float validInputDistance = 1.25f;
     // distance for input to be too early or late
-    public const float offDistance = 0.75f;
+    public const float offDistance = 0.5f;
     // distance to pop note from list
     public const float tooFarDistance = 1f;
-    // number of notes emitted
+    // number of notes deleted
     private int noteCounter;
+    //number of notes shot
+    private int notesShot = 0;
     // total number of notes
     private int numOfNotes;
     // if game over or not
@@ -44,6 +46,9 @@ public class NoteEmitter : MonoBehaviour
     public AudioSource gameOverSfx;
     public AudioSource winSfx;
 
+    // hand and paper objs
+    public GameObject hands;
+    public GameObject paper;
     //animators
     public Animator handAnim;
     public Animator paperAnim;
@@ -51,104 +56,21 @@ public class NoteEmitter : MonoBehaviour
     private int difficulty;
     public int health;
 
-    // Returns true if distance between first note and noteDetector is close enough to process input
-    bool isValidInput()
-    {
-        if (Mathf.Abs(detector.transform.position.x - allNotes[0].transform.position.x) < validInputDistance)
-        {
-            return true;
-        }
-        return false;
-    }
+    //timers
+    private float noteTimer;
 
-    // Returns true if button press is too early
-    bool earlyCheck()
-    {
-        if ((allNotes[0].transform.position.x - detector.transform.position.x) > offDistance)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    // Returns true if button press is too late
-    bool lateCheck()
-    {
-        if ((detector.transform.position.x - allNotes[0].transform.position.x) > offDistance)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    // Returns true if correct button is pressed
-    int successCheck()
-    {
-        Note note = allNotes[0].GetComponent<Note>();
-        if ((note.type == 0 && Input.GetKeyDown("up")) || (note.type == 1 && Input.GetKeyDown("left")) ||
-            (note.type == 2 && Input.GetKeyDown("right")) || (note.type == 3 && Input.GetKeyDown("down")))
-        {
-            return note.type;
-        }
-        return -1;
-    }
-
-    // int PickRandomNote()
-    // {
-
-    // }
-
-    // Takes in note spawn location and speed as arguments
-    void EmitNote(float x, float y, float xV)
-    {
-        if (game_over) return;
-        // Chooses a random note out of the four to use
-        GameObject emittingNote = numberNotes[UnityEngine.Random.Range(0, 4)];
-        // GameObject emittingNote = numberNotes[PickRandomNote()];
-        
-        // Instantiates the note at the given spawn location with no rotation
-        GameObject emittedNote = (GameObject)Instantiate(emittingNote, new Vector3(x, y, 0), Quaternion.identity);
-
-        allNotes.Add(emittedNote);
-
-        // Give the note the specified speed
-        Rigidbody2D rb = emittedNote.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector3(xV, 0, 0);
-    }
-
-    IEnumerator SpitNotes()
-    {
-        // Set Note Spawn Location
-        float xEmit = 9f;
-        float yEmit = detector.transform.position.y;
-
-        // Set Note Detector Location
-        float xDetect = detector.transform.position.x;
-        float yDetect = detector.transform.position.y;
-
-        // Set rate of note emitting
-        // float noteRate = 1f;
-
-        // Calculates necessary note velocity
-        float xDist = xDetect - xEmit;
-        float xVel = xDist / noteTime;
-
-        //int numOfNotes = timeBeforeNotes.Count;
-
-        // Calls the note emitter every x second(s)
-        for(int i=0; i<numOfNotes; i++)
-        {
-            // yield return new WaitForSeconds(noteRate);
-            // print(timeBeforeNotes[i]);
-            yield return new WaitForSeconds(timeBeforeNotes[i]/1000f);
-            EmitNote(xEmit, yEmit, xVel);
-        }
-    }
+    public float bpm;
+    private float bpmTimer;
 
     // Start is called before the first frame update
     void Start()
     {
+        // seconds per beat
+        bpm = 6f/13f;
+        bpmTimer = bpm;
         noteCounter = 0;
+        noteTimer = 0f;
+        notesShot = 0;
         health = 3;
         // Gets note Detector
         detector = GameObject.Find("NotePress");
@@ -157,8 +79,10 @@ public class NoteEmitter : MonoBehaviour
         controller = GameObject.Find("Jam Controller").GetComponent<MicrogameJamController>();
 
         // Initialize Animations
-        handAnim = GameObject.Find("Hands").GetComponent<Animator>();
-        paperAnim = GameObject.Find("Paper").GetComponent<Animator>();
+        hands = GameObject.Find("Hands");
+        handAnim = hands.GetComponent<Animator>();
+        paper = GameObject.Find("Paper");
+        paperAnim = paper.GetComponent<Animator>();
 
         // Gets current difficulty (1, 2, or 3)
         difficulty = controller.GetDifficulty();
@@ -205,17 +129,110 @@ public class NoteEmitter : MonoBehaviour
         numberNotes.Add(1, leftNote);
         numberNotes.Add(2, rightNote);
         numberNotes.Add(3, downNote);
-
-        // Actually starts the note emitting
-        StartCoroutine("SpitNotes");
     }
 
-    // Update is called once per frame
-    void Update()
+    void bpmAnimation() {
+        if (!handAnim.GetCurrentAnimatorStateInfo(0).IsName("default")) {
+            bpmTimer = 0f;
+        }
+        bpmTimer += Time.deltaTime;
+        if (bpmTimer >= bpm) {
+            bpmTimer = 0f;
+            hands.transform.position = new Vector3(0f, 0f, 0f);
+            LeanTween.moveY(hands, hands.transform.position.y + 0.3f, 0.3f).setEaseShake();
+        }
+    }
+    
+    // Returns true if distance between first note and noteDetector is close enough to process input
+    bool isValidInput()
     {
-        if (allNotes.Count <= 0 || game_over) {
+        if (Mathf.Abs(detector.transform.position.x - allNotes[0].transform.position.x) < validInputDistance)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Returns true if button press is too early
+    bool earlyCheck()
+    {
+        if ((allNotes[0].transform.position.x - detector.transform.position.x) > offDistance)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Returns true if button press is too late
+    bool lateCheck()
+    {
+        if ((detector.transform.position.x - allNotes[0].transform.position.x) > offDistance)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Returns true if correct button is pressed
+    int successCheck()
+    {
+        Note note = allNotes[0].GetComponent<Note>();
+        if ((note.type == 0 && Input.GetKeyDown("up")) || (note.type == 1 && Input.GetKeyDown("left")) ||
+            (note.type == 2 && Input.GetKeyDown("right")) || (note.type == 3 && Input.GetKeyDown("down")))
+        {
+            return note.type;
+        }
+        return -1;
+    }
+
+    // Takes in note spawn location and speed as arguments
+    void EmitNote(float x, float y, float xV)
+    {
+        if (game_over) return;
+        // Chooses a random note out of the four to use
+        GameObject emittingNote = numberNotes[UnityEngine.Random.Range(0, 4)];
+        // GameObject emittingNote = numberNotes[PickRandomNote()];
+        
+        // Instantiates the note at the given spawn location with no rotation
+        GameObject emittedNote = (GameObject)Instantiate(emittingNote, new Vector3(x, y, 0), Quaternion.identity);
+        notesShot++;
+        allNotes.Add(emittedNote);
+
+        // Give the note the specified speed
+        Rigidbody2D rb = emittedNote.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(xV, 0, 0);
+    }
+
+    void shootNote() {
+        if (notesShot >= numOfNotes) {
             return;
         }
+        // Set Note Spawn Location
+        float xEmit = 9f;
+        float yEmit = detector.transform.position.y;
+
+        // Set Note Detector Location
+        float xDetect = detector.transform.position.x;
+        float yDetect = detector.transform.position.y;
+
+        // Set rate of note emitting
+        // float noteRate = 1f;
+
+        // Calculates necessary note velocity
+        float xDist = xDetect - xEmit;
+        float xVel = xDist / noteTime;
+
+        // timeBeforeNotes[] is the milliseconds between notes
+        float note_interval = timeBeforeNotes[notesShot]/1000f;
+        noteTimer += Time.deltaTime;
+        if (noteTimer >= note_interval) {
+            noteTimer = 0f;
+            EmitNote(xEmit, yEmit, xVel);
+        }
+    }
+
+    // updates notes
+    void updateNotes() {
         bool success = false;
         // set sprite to incoming sprite, and lower opacity.
         detector.GetComponent<SpriteRenderer>().sprite = allNotes[0].GetComponent<SpriteRenderer>().sprite;
@@ -228,39 +245,39 @@ public class NoteEmitter : MonoBehaviour
         {
             if (isValidInput()) {
                 successState = successCheck();
-                if (earlyCheck())
-                {
-                    print("TOO EARLY, LOSE");
-                    // Lose function or health - 1
+                //earlyCheck
+                if (earlyCheck()) {
+                    //print("TOO EARLY, LOSE");
+                    success = false;               
                 }
-                else if (lateCheck())
-                {
-                    print("TOO LATE, LOSE");
-                    // Lose function or health - 1
+                else if (lateCheck()) {
+                    //print("TOO LATE, LOSE");
+                    success = false;
                 }
-                else if (successState == -1)
-                {
-                    print("WRONG BUTTON, LOSE");
-                    // Lose function or health - 1
+                else if (successState == -1) {
+                    //print("WRONG BUTTON, LOSE");
+                    success = false;  
                 }
-                else
-                {
+                else {
                     success = true;
-                    print("SUCCESS!!!");
+                    //print("SUCCESS!!!");
                     // Success Sound and Visual
                 }
 
                 // play animation and sfx depending on if success or not
                 if (success) {
+                    // tween a direction
                     hitSfx.PlayOneShot(hitSfx.clip, 1f);
+                    playAnimation(successState);
                 }
                 else {
                     missSfx.PlayOneShot(missSfx.clip, 1f);
                     health--;
                     Animator heartsAnim = GameObject.Find("Hearts").GetComponent<Animator>();
                     heartsAnim.Play($"{health}_hearts", -1, 0f);
+                    playAnimation(-1);
                 }
-                playAnimation(successState);
+                
                 if (health <= 0) {
                     gameOver();
                     return;
@@ -274,7 +291,7 @@ public class NoteEmitter : MonoBehaviour
         // Check if note is too far past detector and add to fail condition
         if ((detector.transform.position.x - allNotes[0].transform.position.x) > tooFarDistance)
         {
-            print("TOO FAR!");
+            //print("TOO FAR!");
             // Lose function or health - 1
             missSfx.PlayOneShot(missSfx.clip, 1f);
             health--;
@@ -289,6 +306,16 @@ public class NoteEmitter : MonoBehaviour
             DeleteFrontNote();
             return;
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootNote();
+        if (allNotes.Count > 0) {
+            updateNotes();
+        }
+        bpmAnimation();
     }
 
     // deletes the current front note
@@ -319,6 +346,24 @@ public class NoteEmitter : MonoBehaviour
         else {
             handAnim.Play($"{currentState}_{noteDir[type]}", -1, 0f);
             paperAnim.Play($"{currentState}_{noteDir[type]}", -1, 0f);
+            LeanTween.cancel(hands);
+            hands.transform.position = new Vector3(0f, 0f, 0f);
+            switch (type) {
+                case 0:
+                    LeanTween.moveY(hands, hands.transform.position.y + 0.3f, 0.3f).setEaseShake();
+                    break;
+                case 1:
+                    LeanTween.moveX(hands, hands.transform.position.x - 0.3f, 0.3f).setEaseShake();
+                    break;
+                case 2:
+                    LeanTween.moveX(hands, hands.transform.position.x + 0.3f, 0.3f).setEaseShake();
+                    break;
+                case 3:
+                    LeanTween.moveY(hands, hands.transform.position.y - 0.3f, 0.3f).setEaseShake();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
@@ -326,7 +371,6 @@ public class NoteEmitter : MonoBehaviour
     void gameOver() {
         game_over = true;
         handAnim.SetBool("gameOver", true);
-        paperAnim.SetBool("gameOver", true);
         for (int i = 0; i < allNotes.Count; i++) {
             Destroy(allNotes[i]);
         }
@@ -346,10 +390,19 @@ public class NoteEmitter : MonoBehaviour
 
     void gameWin() {
         game_over = true;
+        for (int i = 0; i < allNotes.Count; i++) {
+            Destroy(allNotes[i]);
+        }
+        allNotes.Clear();
         // play animation where hands move apart
         GameObject.Find("Hands").GetComponent<SpriteRenderer>().enabled = false;
-        GameObject.Find("EndHands").transform.Find("HandL").GetComponent<SpriteRenderer>().enabled = true;
-        GameObject.Find("EndHands").transform.Find("HandR").GetComponent<SpriteRenderer>().enabled = true;
+        GameObject handL = GameObject.Find("EndHands");
+        GameObject handR = GameObject.Find("EndHands");
+        handL.transform.Find("HandL").GetComponent<SpriteRenderer>().enabled = true;
+        handR.transform.Find("HandR").GetComponent<SpriteRenderer>().enabled = true;
+
+        LeanTween.moveX(handL, handL.transform.position.x - 0.2f, 0f);
+        LeanTween.moveX(handR, handR.transform.position.x + 0.2f, 0f);
 
         paperAnim.Play($"win_{difficulty}");
         winSfx.PlayOneShot(winSfx.clip, 1f);
